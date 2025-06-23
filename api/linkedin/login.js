3ec6668c19b1f5e0e4575407ca52578993e394f3
ckakilel@gmail.com <kakilelbarwett@gmail.com>
2025-06-23 11:01:10 +0300

    last linked in

--- api/linkedin/login.js
+++ api/linkedin/login.js
@@ -1,10 +1,11 @@
-// /api/linkedin/login.js
-export default async function handler(req, res) {
-  const CLIENT_ID = process.env.LINKEDIN_CLIENT_ID;
-  const REDIRECT_URI = process.env.LINKEDIN_REDIRECT_URI;
-  const SCOPE = "r_liteprofile r_emailaddress w_member_social";
+export default function handler(req, res) {
+  const clientId = process.env.LINKEDIN_CLIENT_ID;
+  const redirectUri = process.env.LINKEDIN_REDIRECT_URI;
+  const scope = "r_liteprofile r_emailaddress w_member_social";
 
-  const authUrl = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&scope=${encodeURIComponent(SCOPE)}`;
+  const authUrl = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${clientId}&redirect_uri=${encodeURIComponent(
+    redirectUri
+  )}&scope=${encodeURIComponent(scope)}`;
 
   res.redirect(authUrl);
 }
